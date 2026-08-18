6c2c84fd975e14d78adff34ca08b1be39639614b
ryxa <rayhansatriabudi@gmail.com>
2026-08-18 10:04:20 +0700

    feat: display dead folders

--- agustus/python_automation/dead-folder-detector/main.py
+++ agustus/python_automation/dead-folder-detector/main.py
@@ -7,7 +7,7 @@ from abc import ABC, abstractmethod
 class BaseFolderDetector(ABC):
     def __init__(self, root):
         self.root = Path(root)
-        self.dead_folder = []
+        self.dead_folders = []
     @abstractmethod
     def scan(self):
         pass
@@ -18,15 +18,44 @@ class BaseFolderDetector(ABC):
 
     @abstractmethod 
     def delete(self):
-        pass
+        pass    
 
+class DeadFolderDetector(BaseFolderDetector):
+    
+    def scan(self) -> list:
+        folders = []
+        for file in self.root.rglob("*"):
+            if file.is_dir():
+                folders.append(file)
+
+        return folders
+
+    def analyze(self, folder: Path) -> bool:
+        scanning = list(folder.iterdir())
+        return len(scanning) == 1
+
+    def find_dead_folder(self):
+        path_folder = self.scan()
+        self.dead_folders = [f for f in path_folder if self.analyze(f)]
+
+    @property
+    def display(self):
+        print(f"There are {len(self.dead_folders)} folders")
+        for i in range(len(self.dead_folders)):
+            print(f"{i+1} [DEAD] {self.dead_folders[i]}")
 
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <path> [--delete]")
 
+    root = sys.argv[1]
+    program = DeadFolderDetector(root)
+    print(f"Scanning {program.root}")
+    print()
 
+    program.find_dead_folder()
+    program.display
 
 if __name__ == "__main__":
     main()
