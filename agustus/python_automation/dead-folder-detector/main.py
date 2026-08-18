f93a3523b2c1a6bffd44957ce72fb6b73e11bf83
ryxa <rayhansatriabudi@gmail.com>
2026-08-18 09:12:08 +0700

    feat: python CLI parsing

---
+++ agustus/python_automation/dead-folder-detector/main.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import shutil
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path> [--delete]")
+
+if __name__ == "__main__":
+    main()
