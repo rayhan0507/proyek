40558c155bd9876f1d395d9e0a848f6b2d111b9d
ryxa <rayhansatriabudi@gmail.com>
2026-08-18 09:37:43 +0700

    style: adding abstract method pt2

--- agustus/python_automation/dead-folder-detector/main.py
+++ agustus/python_automation/dead-folder-detector/main.py
@@ -16,14 +16,17 @@ class BaseFolderDetector(ABC):
     def analyze(self):
         pass
 
+    @abstractmethod 
+    def delete(self):
+        pass
 
-class DeadFolderDetector(BaseFolderDetector):
-    
+
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <path> [--delete]")
 
-   
+
+
 if __name__ == "__main__":
     main()
